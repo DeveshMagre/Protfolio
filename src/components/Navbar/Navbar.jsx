500169525730fd55f87eee3cecc0a85fc57e99fe
Devesh Magre <deveshmagre1@gmail.com>
2025-01-19 21:24:24 +0530

    protfolio

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom"; 
-import Logo from "../../assets/logo.svg";
+import Logo from "../../assets/Logo.svg";
 import { motion } from "framer-motion";
 
 const NavLinks = [
